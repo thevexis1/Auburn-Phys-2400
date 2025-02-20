a4a043ce2baae444493b528175e493f15c5fc09e
Bradley Andrew <thevexis@gmail.com>
2025-02-20 10:06:39 -0600

    updated class assignment

--- Linear_Algebra/lin_alg_practice.py
+++ Linear_Algebra/lin_alg_practice.py
@@ -47,6 +47,21 @@ print(f"xi = {solution[0]:.2f}, eta = {solution[1]:.2f}, zeta = {solution[2]:.2f
 
 #%%
 
+C2 = np.array([[2, -1, 3, 8, 1],
+              [1, 3, -2, 0, 3],
+              [4, -3, 1, 4, -5],
+              [6, 6, 1, 0, -9],
+              [8, 9, 1, 1, 5]])  # Coefficients of xi, eta, zeta
+
+b2 = np.array([5, -3, 4, 8, 12])  # Right-hand side values
+
+solution2 = np.linalg.solve(C2, b2)  # Solve for [xi, eta, zeta]
+
+print("Solving Larger System of Equations")
+print(f"xi = {solution2[0]:.2f}, eta = {solution2[1]:.2f}, zeta = {solution2[2]:.2f}, theta = = {solution2[3]:.2f}, beta = = {solution2[4]:.2f}")
+
+#%%
+
 """
 
 This section will be for you to solve the next two sections of the assignment
